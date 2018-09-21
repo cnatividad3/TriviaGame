a3d548c0a8e6e1dc776e17510b79dcb8deeab328
Chris Natividad <cnat@Chriss-MacBook-Pro.local>
2018-09-21 16:23:52 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,39 @@
+
 //variables
 var questionsRight = 0;
 var questionsWrong = 0;
 
 //timer
-var sec = 1;
+var sec = 15;
 var timer = setInterval(gameTimer, 1000);
 
 function gameTimer() {
-    document.getElementById('timer').innerHTML = sec + "sec left";
-    sec--;
-    if (sec == -1) {
-        clearInterval(timer);
-        alert("Time is up!!");
-    }
+  document.getElementById('timer').innerHTML = sec + "sec left";
+  sec--;
+  if (sec == -1) {
+    clearInterval(timer);
+    alert("Time is up!!");
+  }
 }
 
+
+
+
 //store answers
+var firstQuestion = document.getElementsByName('question1');
+
+for (var i = 0; i < firstQuestion.length; i++)
+{
+ if (firstQuestion[i].checked)
+ {
+  //??? 
+firstQuestion[i].value
+
+  
+  break;
+ }
+}
 
 
-//display answers
+
+//display answer count
